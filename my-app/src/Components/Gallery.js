b37d136f095961acd19851bb68d93f50def8cbda
muirisc <91299876+muirisc@users.noreply.github.com>
2022-03-31 22:55:25 -0400

    Further Gallery Work

--- my-app/src/Components/Gallery.js
+++ my-app/src/Components/Gallery.js
@@ -9,16 +9,16 @@ const Gallery = () => {
 {image:"https://cdn.pixabay.com/photo/2017/12/28/23/41/car-3046424_960_720.jpg"},{image:"https://cdn.pixabay.com/photo/2017/02/27/08/27/car-2102367_960_720.jpg"}]
 
   return(
-    <div className="gallery">
-    <h3> Gallery</h3>
-    <div className="galleryIntro">
-    <p>At Ace Powder Coating, we take pride in our work. Here is just some of the work we have done over the years.</p>
-    </div>
-    <div>
+  <div className="gallery">
+        <h3> Gallery</h3>
+     <div className="galleryIntro">
+        <p>At Ace Powder Coating, we take pride in our work. Here is just some of the work we have done over the years.</p>
+     </div>
+    <div className="imageCarousel">
     {imageBank.map((image) => {
     return <GalleryCarousel image={image.image} />;
     })}
-    </div>
+     </div>
     </div>
   )
 }
